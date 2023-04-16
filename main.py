8158949a3c34ddb8c4b6a9bc50a996ee8efd4ac6
claneave <claneave28@gmail.com>
2023-04-16 19:09:36 -0400

    jit done

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ from matplotlib import pyplot as plt
 import json
 import global_classes.data_builder as inputs
 import collections
+import sys
 
-execution_results = collections.defaultdict(dict)
+execution_results_standard = collections.defaultdict(dict)
+execution_results_jit = collections.defaultdict(dict)
 result_field_headers = ['starting_store', 'starting_warehouse', 'starting_manufacturer', 'customer_demand']
 
 
@@ -19,7 +21,19 @@ def build_data_payload(payload):
     return input_values
 
 
-def start_build(days, payload):
+def start_build(days, payload, model):
+    print("Executing testing on variables:\n")
+    print(json.dumps(payload, indent=4, sort_keys=True))
+    if model == 'standard':
+        return build_standard_data(days, payload)
+    elif model == 'jit':
+        return build_jit_data(days, payload)
+    else:
+        print("Invalid selection on model, exiting program")
+        sys.exit(1)
+
+
+def build_jit_data(days, payload):
     max_demand = payload['customer_demand']
     reorder_store_quantity = payload['reorder_store_values']
     reorder_manufacture_quantity = payload['reorder_manufacture_values']
@@ -27,8 +41,9 @@ def start_build(days, payload):
     shipping_store_days = payload['store_shipping']
     shipping_warehouse_days = payload['warehouse_shipping']
     manufacturing_days = payload['manufacturer_shipping']
-    print("Executing testing on variables:\n")
-    print(json.dumps(payload, indent=4, sort_keys=True))
+    trigger_order_store = 0
+    trigger_order_warehouse = 0
+    trigger_order_manufacturer = 0
 
     for day in range(1, days):
         store_order = 0
@@ -42,16 +57,81 @@ def start_build(days, payload):
                         value = value - max_demand
                         if value < 1:
                             value = 0
-                    execution_results[day].update({key: value})
+                    execution_results_jit[day].update({key: value})
         else:
             day = day - 1
-            for key, value in execution_results[day].items():
+            for key, value in execution_results_jit[day].items():
                 if key in result_field_headers:
                     if key == 'customer_demand':
                         value = random_customer_demand(max_demand)
                     if key == 'starting_store':
                         if value > 1:
-                            value = value - execution_results[day]['customer_demand']
+                            value = value - execution_results_jit[day]['customer_demand']
+                        else:
+                            value = 0
+                            if trigger_order_store >= shipping_store_days:
+                                value += reorder_store_quantity
+                                store_order = reorder_store_quantity
+                                trigger_order_store = 0
+                            else:
+                                trigger_order_store += 1
+                    if key == 'starting_warehouse':
+                        if value > 1:
+                            value = value - store_order
+                        else:
+                            value = 0
+                            if trigger_order_warehouse >= shipping_warehouse_days:
+                                value += reorder_warehouse_quantity
+                                warehouse_order = reorder_warehouse_quantity
+                                trigger_order_warehouse = 0
+                            else:
+                                trigger_order_warehouse += 1
+                    if key == 'starting_manufacture':
+                        if value > 1:
+                            value = value - warehouse_order
+                        else:
+                            value = 0
+                            if trigger_order_manufacturer >= manufacturing_days:
+                                value += reorder_manufacture_quantity
+                                trigger_order_store = 0
+                            else:
+                                trigger_order_store += 1
+                    if value < 1:
+                        value = 0
+                    execution_results_jit[day + 1].update({key: value})
+    return execution_results_jit
+
+
+def build_standard_data(days, payload):
+    max_demand = payload['customer_demand']
+    reorder_store_quantity = payload['reorder_store_values']
+    reorder_manufacture_quantity = payload['reorder_manufacture_values']
+    reorder_warehouse_quantity = payload['reorder_warehouse_values']
+    shipping_store_days = payload['store_shipping']
+    shipping_warehouse_days = payload['warehouse_shipping']
+    manufacturing_days = payload['manufacturer_shipping']
+    for day in range(1, days):
+        store_order = 0
+        warehouse_order = 0
+        if day == 1:
+            for key, value in payload.items():
+                if key in result_field_headers:
+                    if key == 'customer_demand':
+                        value = random_customer_demand(max_demand)
+                    if key == 'starting_store':
+                        value = value - max_demand
+                        if value < 1:
+                            value = 0
+                    execution_results_standard[day].update({key: value})
+        else:
+            day = day - 1
+            for key, value in execution_results_standard[day].items():
+                if key in result_field_headers:
+                    if key == 'customer_demand':
+                        value = random_customer_demand(max_demand)
+                    if key == 'starting_store':
+                        if value > 1:
+                            value = value - execution_results_standard[day]['customer_demand']
                         else:
                             value = 0
                         if day % shipping_store_days == 0:
@@ -70,8 +150,8 @@ def start_build(days, payload):
                             value += reorder_manufacture_quantity
                     if value < 1:
                         value = 0
-                    execution_results[day + 1].update({key: value})
-    return execution_results
+                    execution_results_standard[day + 1].update({key: value})
+    return execution_results_standard
 
 
 def inventory_processing(inventory, inventory_removed):
@@ -84,6 +164,7 @@ def random_customer_demand(max_demand):
 
 def build_plot_graph(results):
     df = pd.DataFrame.from_dict(results)
+    df.style.set_caption('Model Results')
     plt.plot(df)
     plt.show()
     return df
@@ -95,10 +176,12 @@ if __name__ == '__main__':
     cost_of_delay_per_unit = input_value_dict['cost_delayed']
     cost_of_unit = input_value_dict['cost_unit']
     cost_of_instock_storage = input_value_dict['cost_instock']
-    execution_results = start_build(days, input_value_dict)
-    plot_graph = build_plot_graph(execution_results)
-    print(f"\n{plot_graph}")
-    gather_metrics = calculations.Calculations(total_days=days, results=execution_results,
-                                               delay_cost=cost_of_delay_per_unit, unit_cost=cost_of_unit,
-                                               storage_cost=cost_of_instock_storage).gather_all_metrics()
-    print(f"\n{gather_metrics}")
+    execution_results_standard = start_build(days, input_value_dict, 'standard')
+    execution_results_jit = start_build(days, input_value_dict, 'jit')
+    for execution in execution_results_jit,execution_results_standard:
+        plot_graph = build_plot_graph(execution)
+        print(f"\n{plot_graph}")
+        gather_metrics = calculations.Calculations(total_days=days, results=execution,
+                                                   delay_cost=cost_of_delay_per_unit, unit_cost=cost_of_unit,
+                                                   storage_cost=cost_of_instock_storage).gather_all_metrics()
+        print(f"\n{gather_metrics}")
