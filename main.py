d99e9608ec30b1ad01054d6ea0b3a32755a8daea
claneave <claneave28@gmail.com>
2023-04-16 14:25:44 -0400

    processing properly for data now

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import numpy as np
 import pandas
 from matplotlib import pyplot as plt
 import json
-import global_classes.data_executor as inputs
+import global_classes.data_builder as inputs
+import collections
 
+execution_results = collections.defaultdict(dict)
 
 def build_data_payload(payload):
     if payload:
@@ -14,8 +16,14 @@ def build_data_payload(payload):
     return input_values
 
 
+def start_build(days, payload):
+    for day in range(1, days):
+        for key, value in payload.items():
+            print(key, value)
+            execution_results[day].update({key: value})
+    print(execution_results)
+
+
 if __name__ == '__main__':
     input_value_dict = build_data_payload(False)
-    print(input_value_dict)
-
-
+    start_build(100, input_value_dict)
