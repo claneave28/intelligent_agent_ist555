5993865f7aa095d8e907084677a454934c8b2c45
claneave <claneave28@gmail.com>
2023-04-16 13:35:55 -0400

    finished dataexecutor class for data input

--- global_classes/data_executor.py
+++ global_classes/data_executor.py
@@ -12,13 +12,13 @@ class DataExecutor(object):
         self._list_of_inventory_trigger_reorders = ['store', 'warehouse']
         self._list_of_shipping_failure_rates = ['shipping_failure']
         self._list_of_customer_demand = ['customer_demand']
-        self.dictionary_values = kwargs
+        self.defined_dictionary_values = kwargs
         self.dictionary_of_input_variables = {}
         self.build_data_points = self.build_data_points()
 
     def build_data_points(self):
         if self._random_inputs:
-            # start with cost of inventory building
+            # Building Out random inputs for each value unless otherwise stated explicitly in the original configuration
             for cost in self._list_cost_of_inventory:
                 self.dictionary_of_input_variables[cost] = self.randomize_input_numeric_value(1, 5)
             for shipping_times in self._list_shipping_times:
@@ -38,6 +38,20 @@ class DataExecutor(object):
                 else:
                     self.dictionary_of_input_variables[starting_inventory] = self.randomize_input_numeric_value(1000,
                                                                                                                 10000)
+            for trigger_reorder in self._list_of_inventory_trigger_reorders:
+                if trigger_reorder is 'store':
+                    self.dictionary_of_input_variables[trigger_reorder] = self.randomize_input_numeric_value(25, 100)
+                else:
+                    self.dictionary_of_input_variables[trigger_reorder] = self.randomize_input_numeric_value(100, 250)
+
+            for shipping_failure in self._list_of_shipping_failure_rates:
+                self.dictionary_of_input_variables[shipping_failure] = self.randomize_input_numeric_value(0, 2)
+
+            for customer_demand in self._list_of_customer_demand:
+                self.dictionary_of_input_variables[customer_demand] = self.randomize_input_numeric_value(75, 100)
+        else:
+            self.dictionary_of_input_variables = self.defined_dictionary_values
+        return self.dictionary_of_input_variables
 
     @staticmethod
     def randomize_input_numeric_value(numeric_range_low, numeric_range_high):
