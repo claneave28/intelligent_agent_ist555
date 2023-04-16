18b5b2a6da2ff7d0d17c7376b06c663588fbc80e
claneave <claneave28@gmail.com>
2023-04-16 15:41:07 -0400

    works

--- global_classes/data_builder.py
+++ global_classes/data_builder.py
@@ -9,6 +9,8 @@ class DataExecutor(object):
         self._list_shipping_times = ['store_shipping', 'warehouse_shipping', 'manufacturer_shipping']
         self._list_of_starting_inventory = ['starting_store', 'starting_warehouse', 'starting_manufacturer']
         self._list_of_inventory_trigger_reorders = ['reorder_store', 'reorder_warehouse']
+        self._list_of_reorder_values = ['reorder_store_values', 'reorder_warehouse_values',
+                                        'reorder_manufacture_values']
         self._list_of_shipping_failure_rates = ['shipping_failure']
         self._list_of_customer_demand = ['customer_demand']
         self.dictionary_of_input_variables = {}
@@ -18,7 +20,8 @@ class DataExecutor(object):
         # Building Out random inputs for each value unless otherwise stated explicitly in the original configuration
         for cost in self._list_cost_of_inventory:
             self.dictionary_of_input_variables[cost] = self.randomize_input_numeric_value(1, 5)
-        if self.dictionary_of_input_variables['cost_unit'] <= self.dictionary_of_input_variables['cost_delayed'] or self.dictionary_of_input_variables['cost_unit'] <= self.dictionary_of_input_variables['cost_instock']:
+        if self.dictionary_of_input_variables['cost_unit'] <= self.dictionary_of_input_variables['cost_delayed'] or \
+                self.dictionary_of_input_variables['cost_unit'] <= self.dictionary_of_input_variables['cost_instock']:
             self.dictionary_of_input_variables['cost_unit'] += 2
         for shipping_times in self._list_shipping_times:
             if shipping_times == 'store_shipping':
@@ -27,6 +30,16 @@ class DataExecutor(object):
                 self.dictionary_of_input_variables[shipping_times] = self.randomize_input_numeric_value(6, 10)
             else:
                 self.dictionary_of_input_variables[shipping_times] = self.randomize_input_numeric_value(11, 15)
+        for reorder_values in self._list_of_reorder_values:
+            if reorder_values == 'reorder_store_values':
+                self.dictionary_of_input_variables[reorder_values] = self.randomize_input_numeric_value(100,
+                                                                                                        200)
+            if reorder_values == 'reorder_warehouse_values':
+                self.dictionary_of_input_variables[reorder_values] = self.randomize_input_numeric_value(200,
+                                                                                                        500)
+            else:
+                self.dictionary_of_input_variables[reorder_values] = self.randomize_input_numeric_value(1000,
+                                                                                                        10000)
         for starting_inventory in self._list_of_starting_inventory:
             if starting_inventory == 'starting_store':
                 self.dictionary_of_input_variables[starting_inventory] = self.randomize_input_numeric_value(100,
--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import global_classes.data_builder as inputs
 import collections
 
 execution_results = collections.defaultdict(dict)
-
+result_field_headers = ['starting_store','starting_warehouse','starting_manufacturer','customer_demand']
 
 def build_data_payload(payload):
     if payload:
@@ -21,28 +21,46 @@ def build_data_payload(payload):
 
 def start_build(days, payload):
     max_demand = payload['customer_demand']
-    starting_store_inventory = payload['starting_store']
-    starting_warehouse_inventory = payload['starting_warehouse']
-    starting_manufacturer_inventory = payload['starting_manufacturer']
+    reorder_store_quantity = payload['reorder_store_values']
+    reorder_manufacture_quantity = payload['reorder_manufacture_values']
+    reorder_warehouse_quantity = payload['reorder_warehouse_values']
+    shipping_store_days = payload['store_shipping']
+    shipping_warehouse_days = payload['warehouse_shipping']
+    manufacturing_days = payload['manufacturer_shipping']
 
     for day in range(1, days):
+        store_order = 0
+        warehouse_order = 0
         if day == 1:
             for key, value in payload.items():
-                if key == 'customer_demand':
-                    value = random_customer_demand(max_demand)
-                if key == 'starting_store':
-                    value = value - max_demand
-                execution_results[day].update({key: value})
+                if key in result_field_headers:
+                    if key == 'customer_demand':
+                        value = random_customer_demand(max_demand)
+                    if key == 'starting_store':
+                        value = value - max_demand
+                    execution_results[day].update({key: value})
         else:
             day = day - 1
             for key, value in execution_results[day].items():
-                if key == 'customer_demand':
-                    value = random_customer_demand(max_demand)
-                if key == 'starting_store':
-                    value = value - execution_results[day]['customer_demand']
-
-                execution_results[day+1].update({key: value})
-        print(execution_results)
+                if key in result_field_headers:
+                    if key == 'customer_demand':
+                        value = random_customer_demand(max_demand)
+                    if key == 'starting_store':
+                        value = value - execution_results[day]['customer_demand']
+                        if day % shipping_store_days:
+                            value += reorder_store_quantity
+                            store_order = reorder_store_quantity
+                    if key == 'starting_warehouse':
+                        value = value - store_order
+                        if day % shipping_warehouse_days:
+                            value += reorder_warehouse_quantity
+                            warehouse_order = reorder_warehouse_quantity
+                    if key == 'starting_manufacture':
+                        value = value - warehouse_order
+                        if day % manufacturing_days:
+                            value += reorder_manufacture_quantity
+                    execution_results[day+1].update({key: value})
+    print(execution_results)
 
 
 def inventory_processing(inventory, inventory_removed):
@@ -55,4 +73,4 @@ def random_customer_demand(max_demand):
 
 if __name__ == '__main__':
     input_value_dict = build_data_payload(False)
-    start_build(5, input_value_dict)
+    start_build(15, input_value_dict)
