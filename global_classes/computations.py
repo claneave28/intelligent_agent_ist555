2663d7ec3d7ebd141fc143b39902b2facff6671c
claneave <claneave28@gmail.com>
2023-04-22 19:08:24 -0400

    added html documentation

--- global_classes/computations.py
+++ global_classes/computations.py
@@ -2,8 +2,48 @@ import statistics
 
 
 class Calculations(object):
+    """The summary line for a class docstring should fit on one line.
 
+    If the class has public attributes, they may be documented here
+    in an ``Attributes`` section and follow the same formatting as a
+    function's ``Args`` section. Alternatively, attributes may be documented
+    inline with the attribute's declaration (see __init__ method below).
+
+    Properties created with the ``@property`` decorator should be documented
+    in the property's getter method.
+
+    Attributes
+    ----------
+    attr1 : str
+        Description of `attr1`.
+    attr2 : :obj:`int`, optional
+        Description of `attr2`.
+
+    """
     def __init__(self, total_days, results, delay_cost, unit_cost, storage_cost):
+        """Example of docstring on the __init__ method.
+
+        The __init__ method may be documented in either the class level
+        docstring, or as a docstring on the __init__ method itself.
+
+        Either form is acceptable, but the two should not be mixed. Choose one
+        convention to document the __init__ method and be consistent with it.
+
+        Note
+        ----
+        Do not include the `self` parameter in the ``Parameters`` section.
+
+        Parameters
+        ----------
+        param1 : str
+            Description of `param1`.
+        param2 : :obj:`list` of :obj:`str`
+            Description of `param2`. Multiple
+            lines are supported.
+        param3 : :obj:`int`, optional
+            Description of `param3`.
+
+        """
         self._result_payload = results
         self._total_days = total_days
         self._store_inventory = []
@@ -16,6 +56,55 @@ class Calculations(object):
         self._calculated_results = {}
 
     def gather_all_metrics(self):
+        """Module
+
+        Function parameters should be documented in the ``Parameters`` section.
+        The name of each parameter is required. The type and description of each
+        parameter is optional, but should be included if not obvious.
+
+        If \*args or \*\*kwargs are accepted,
+        they should be listed as ``*args`` and ``**kwargs``.
+
+        The format for a parameter is::
+
+            name : type
+                description
+
+                The description may span multiple lines. Following lines
+                should be indented to match the first line of the description.
+                The ": type" is optional.
+
+                Multiple paragraphs are supported in parameter
+                descriptions.
+
+        Parameters
+        ----------
+        param1 : int
+            The first parameter.
+        param2 : :obj:`str`, optional
+            The second parameter.
+        *args
+            Variable length argument list.
+        **kwargs
+            Arbitrary keyword arguments.
+
+        Returns
+        -------
+        bool
+            True if successful, False otherwise.
+
+            The return type is not optional. The ``Returns`` section may span
+            multiple lines and paragraphs. Following lines should be indented to
+            match the first line of the description.
+
+            The ``Returns`` section supports any reStructuredText formatting,
+            including literal blocks::
+
+                {
+                    'param1': param1,
+                    'param2': param2
+                }
+                """
         for key, value in self._result_payload.items():
             if value['starting_store'] == 0:
                 self._missed_sales_requests.append(value['customer_demand'])
